bc7748965022782c1d911f419652a045e8e58058
Allen Royston <allen.acr@gmail.com>
2017-02-17 14:59:49 -0800

    updated to pug and node components

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var site = require('./routes/index'),
 var app = express();
 
 // view engine setup
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
